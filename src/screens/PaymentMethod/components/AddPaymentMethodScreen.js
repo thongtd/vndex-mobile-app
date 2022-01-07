1cce1cb3ab0fa9590be8dff467abfac218abc86d
toan sajxuko <toan@wowpay.io>
2022-01-08 00:21:10 +0700

    clear UI payment method

--- src/screens/PaymentMethod/components/AddPaymentMethodScreen.js
+++ src/screens/PaymentMethod/components/AddPaymentMethodScreen.js
@@ -20,33 +20,70 @@ const AddPaymentMethodScreen = ({componentId, typeScreen = ''}) => {
         return 'Thêm ví MoMo';
       case constant.TYPE_PAYMENT_SCREEN.ADD_VIETTEL_PAY:
         return 'Thêm ví Viettel Pay';
-      // default:
-      //   return 'Thêm chuyển khoản ngân hàng';
     }
   };
-  
+
+  const renderLayout = () => {
+    switch (typeScreen) {
+      case constant.TYPE_PAYMENT_SCREEN.ADD_BANKING:
+        return (
+          <>
+            <Input placeholder="LU TUAN ANH" isLabel label="Tên *" />
+            <Input
+              placeholder="Nhập số tài khoản ngân hàng"
+              isLabel
+              label="Tài khoản ngân hàng/ Số thẻ *"
+            />
+            <Input
+              placeholder="Nhập tên ngân hàng"
+              isLabel
+              label="Tên ngân hàng *"
+            />
+            <Input
+              placeholder="Nhập thông tin chi nhánh ngân hàng"
+              isLabel
+              label="Chi nhánh"
+            />
+          </>
+        );
+      case constant.TYPE_PAYMENT_SCREEN.ADD_MOMO:
+        return (
+          <>
+            <Input placeholder="LU TUAN ANH" isLabel label="Họ tên" />
+            <Input
+              placeholder="Nhập số điện thoại của bạn"
+              isLabel
+              label="Số điện thoại"
+            />
+          </>
+        );
+      case constant.TYPE_PAYMENT_SCREEN.ADD_VIETTEL_PAY:
+        return (
+          <>
+            <Input placeholder="LU TUAN ANH" isLabel label="Họ tên *" />
+            <Input
+              placeholder="Nhập địa chỉ email của bạn"
+              type="email"
+              isLabel
+              label="Email"
+            />
+            <Input
+              placeholder="Nhập địa chỉ email của bạn"
+              isLabel
+              label="Số điện thoại"
+            />
+          </>
+        );
+    }
+  };
+
   return (
     <Container
       componentId={componentId}
       isScroll
       title={getTitleScreen() || ''}>
       <>
-        <Input placeholder="LU TUAN ANH" isLabel label="Tên *" />
-        <Input
-          placeholder="Nhập số tài khoản ngân hàng"
-          isLabel
-          label="Tài khoản ngân hàng/ Số thẻ *"
-        />
-        <Input
-          placeholder="Nhập tên ngân hàng"
-          isLabel
-          label="Tên ngân hàng *"
-        />
-        <Input
-          placeholder="Nhập thông tin chi nhánh ngân hàng"
-          isLabel
-          label="Chi nhánh"
-        />
+        {renderLayout()}
         <Layout space={10} isSpaceBetween>
           <View style={{paddingRight: 10, marginTop: 3}}>
             <Icon iconComponent={icons.IcNote} />
