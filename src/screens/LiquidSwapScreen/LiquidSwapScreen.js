3d3a06db6aed757c8776c39a69a998145b172fa8
toan <toan@wowpay.io>
2022-01-17 00:33:02 +0700

    update webview http

--- src/screens/LiquidSwapScreen/LiquidSwapScreen.js
+++ src/screens/LiquidSwapScreen/LiquidSwapScreen.js
@@ -1,22 +1,42 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Button from '../../components/Button/Button'
-import { LOGIN_SCREEN, pushSingleScreenApp } from '../../navigation'
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Button from '../../components/Button/Button';
+import {LOGIN_SCREEN, pushSingleScreenApp} from '../../navigation';
+import WebView from 'react-native-webview';
+import Container from '../../components/Container';
+const url = 'http://dicvietnam.vn/services.html#issues';
+const DicWebview = ({componentId}) => {
+  return (
+    <WebView
+      source={{uri: url}}
+      style={{flex: 1}}
+      scalesPageToFit={true}
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+    />
+  );
+};
+const LiquidSwapScreen = ({componentId}) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  if (isLoaded) {
+    return <DicWebview />;
+  }
+  return (
+    <Container
+      isLoadding={isLoaded}
+      componentId={componentId}
+      title="Smart Issues"
+      isTopBar>
+      <WebView
+        source={{uri: url}}
+        onLoad={() => {
+          setIsLoaded(true);
+        }}
+      />
+    </Container>
+  );
+};
 
-const LiquidSwapScreen = ({
-    componentId
-}) => {
-    return (
-        <View>
-            <Text>Liquid Swap</Text>
-            <Button 
-            isNormal
-            onPress={()=>pushSingleScreenApp(componentId,LOGIN_SCREEN)}
-            />
-        </View>
-    )
-}
+export default LiquidSwapScreen;
 
-export default LiquidSwapScreen
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/screens/StoScreen/StoScreen.js
+++ src/screens/StoScreen/StoScreen.js
@@ -1,14 +1,48 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState} from 'react';
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
+import WebView from 'react-native-webview';
+import Container from '../../components/Container';
 
-const StoScreen = () => {
-    return (
-        <View>
-            <Text>Sto screen</Text>
-        </View>
-    )
-}
+const url = 'http://dicvietnam.vn/services.html#issues';
+const DicWebview = () => {
+  return (
+    <WebView
+      source={{uri: url}}
+      style={{flex: 1}}
+      scalesPageToFit={true}
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+    />
+  );
+};
+const StoScreen = ({componentId}) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  if (isLoaded) {
+    return <DicWebview />;
+  }
+  return (
+    <Container
+      componentId={componentId}
+      title="Smart Issues"
+      isTopBar
+      isLoadding={isLoaded}>
+      <WebView
+        source={{uri: url}}
+        onLoad={() => {
+          setIsLoaded(true);
+        }}
+      />
+    </Container>
+  );
+};
 
-export default StoScreen
+export default StoScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
